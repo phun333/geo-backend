ec44757010f18ee4fa81d35cdf852fe1f7e235f5
phun <149762185+phun333@users.noreply.github.com>
2025-07-01 13:19:54 +0300

    Add ApiResponse model to standardize API responses

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace geoproject.Models
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }    
+        public T? Data { get; set; } // T -> generic type
+    }
+}
