07ef1c9f42373a312cbe2f4722580f9cb1bbafa3
phun <149762185+phun333@users.noreply.github.com>
2025-07-02 11:46:47 +0300

    Add dependency injection for point services and repository in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
+using geoproject.Interfaces;
+using geoproject.Models;
+using geoproject.Services;
+using geoproject.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// saved the repository as a singleton
+// Singleton, Scoped, Transient -> Dependency Injection lifetime management
+//! if we dont use one repo for saving points, the all stats always will be empty
+builder.Services.AddSingleton<IPointRepository, PointRepository>();
+
+// saved the services as scoped
+// for dependency injection
+builder.Services.AddScoped<IPointGetAllService, PointGetAllService>();
+builder.Services.AddScoped<IPointAddService, PointAddService>();
+builder.Services.AddScoped<IPointGetByIdService, PointGetByIdService>();
+builder.Services.AddScoped<IPointUpdateService, PointUpdateService>();
+builder.Services.AddScoped<IPointDeleteService, PointDeleteService>();
 
 // Add services to the container.
 
@@ -17,6 +35,9 @@ app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
+
+//! i will remove this usage when i publish the project
+// this is just for testing purposes
 
 app.Run("http://localhost:5000");
 
