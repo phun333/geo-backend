dd43a52081522157ab2fbc8452238dbb5e6946c9
phun <149762185+phun333@users.noreply.github.com>
2025-07-02 14:40:40 +0300

    Remove commented-out code from Point class for cleaner implementation

--- Models/Point.cs
+++ Models/Point.cs
@@ -1,9 +1,7 @@
 namespace geoproject.Models
 {
     public class Point
-    {
-        // id , pointx pointy,name
-        
+    {        
         public int Id { get; set; }
         public double PointX { get; set; }
         public double PointY { get; set; }
