1bda7524acd69a514f65b1d7e16d832497446298
phun <149762185+phun333@users.noreply.github.com>
2025-07-23 11:36:46 +0300

    Add migration to insert heptagon seed data into Points table, including geometry and name. Update model to reflect new data entry.

---
+++ Migrations/20250710104820_AddHeptagonSeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace geoproject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHeptagonSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Points",
+                columns: new[] { "Id", "CoordinateType", "Geometry", "Name" },
+                values: new object[] { 5, 3, "35.0 41.0, 36.0 41.5, 36.5 42.5, 36.0 43.5, 35.0 44.0, 34.0 43.0, 34.5 42.0, 35.0 41.0", "Heptagon Example" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Points",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
---
+++ Migrations/20250723082415_UpdateCoordinateTypeNullable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace geoproject.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCoordinateTypeNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CoordinateType",
+                table: "Points",
+                type: "integer",
+                nullable: true,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CoordinateType",
+                table: "Points",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true,
+                oldDefaultValue: 1);
+        }
+    }
+}
